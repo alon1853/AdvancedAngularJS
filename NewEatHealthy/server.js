302937e4e657ac4bd2f1b635e383336e61a163a3
Alon <alon1853@gmail.com>
2018-03-04 22:38:41 +0200

    Add Node.JS http request handlers

--- NewEatHealthy/server.js
+++ NewEatHealthy/server.js
@@ -4,4 +4,21 @@ const PORT = 3000
 
 app.use(express.static('app/src'))
 
+app.get('/posts', function (req, res) {
+	const posts = [{
+			id: 1,
+			name: 'sdfsdf'
+	}]
+	
+	res.send(JSON.stringify(posts))
+})
+
+app.get('/posts/:id', function (req, res) {
+	res.send(req.params.id)
+})
+
+app.delete('/posts/:id', function (req, res) {
+	res.send(req.params.id)
+})
+
 app.listen(PORT, () => console.log('Server is running on port ' + PORT + '...'))
