c2aa26e71367723881b6e45a3423a368e5dc5288
Hanan <hananbs158@gmail.com>
2018-03-06 00:21:02 +0200

    Quires:need to finish post and test delete

--- NewEatHealthy/server.js
+++ NewEatHealthy/server.js
@@ -1,13 +1,71 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
+const bodyParser = require('body-parser');
 const app = express()
-const PORT = 3020
+const PORT = 3015
+const url = "mongodb://localhost:27017/"
 
- app.use(express.static('app/src'))
+app.use(express.static('app/src'))
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/get_post_by_id', function (req, res) {
+    var postid = req.body.id;
+    console.log(postid);
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("EatHealthy");
+    dbo.collection("Posts").aggregate([
+        { $match: {
+            _id:ObjectID(req.params.id)
+        }
+        },
+        { $lookup:
+           {
+             from: 'Clients',
+             localField: 'ClientID',
+             foreignField: '_id',
+             as: 'client'
+           }
+         },
+         { $lookup:
+            {
+              from: 'Categories',
+              localField: 'CategoryID',
+              foreignField: '_id',
+              as: 'category'
+            }
+          },
+          { 
+            $project : { 
+                "_id" : 1,
+                "Title": 1,
+                "Content": 1,
+                "client.Name" : { "$arrayElemAt": [ "$client", 0 ] },
+                "category" : { "$arrayElemAt": [ "$category", 0 ] },
+                "CreationDate" : 1
+            }
+            },
+            { 
+                $project : { 
+                    "_id" : 1,
+                    "Title": 1,
+                    "Content": 1,
+                    "client.FirstName" : 1,
+                    "category.Name" : 1,
+                    "CreationDate" : 1
+            },
+        }]).toArray(function(err, result) {
+    if (err) throw err;
+    res.json(result);
+    console.log(result);
+    db.close();
+    });
+    });
+})
 
 app.get('/posts', function (req, res) {
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017/";
-
     MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("EatHealthy");
@@ -33,30 +91,53 @@ app.get('/posts', function (req, res) {
                 "_id" : 1,
                 "Title": 1,
                 "Content": 1,
-                "client.FirstName" : 1,
-                "category.Name" : 1,
+                "client.Name" : { "$arrayElemAt": [ "$client", 0 ] },
+                "category" : { "$arrayElemAt": [ "$category", 0 ] },
                 "CreationDate" : 1
-            } 
+            }
+            },
+            { 
+                $project : { 
+                    "_id" : 1,
+                    "Title": 1,
+                    "Content": 1,
+                    "client.FirstName" : 1,
+                    "category.Name" : 1,
+                    "CreationDate" : 1
+            },
         }]).toArray(function(err, result) {
     if (err) throw err;
-    else res.json(result);
+    res.json(result);
     console.log(result);
     db.close();
     });
     });
 })
 
-app.post("/get_users_list", function(req, res) {
-    var body = req.body;
+app.get("/clients", function(req, res) {
+    //var body = req.body;
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("EatHealthy");
+    dbo.collection("Clients").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+        db.close();
+      });
 });
-
-app.get('/posts/:id', function (req, res) {
-
-	res.send(req.params.id)
 })
 
 app.delete('/posts/:id', function (req, res) {
-	res.send(req.params.id)
+    MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("EatHealthy");
+    dbo.collection("Posts").deleteOne({_id : req.params.id}, function(err, obj) {
+        if (err) throw err;
+        console.log("Post Document deleted");
+        db.close();
+      });
+});
 })
 
 //app.listen(3000, () => console.log('Example app listening on port 3000!'))
