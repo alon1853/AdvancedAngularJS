cec78158d88e75f8c8c886310c68adef78d41c96
sharon77242 <sharon10vp614@gmail.com>
2018-03-07 22:04:22 +0200

    sharone: fix creating post in client

--- NewEatHealthy/app/src/controllers/recommendedPosts.js
+++ NewEatHealthy/app/src/controllers/recommendedPosts.js
@@ -1,22 +1,25 @@
-app.controller("RecommendedPostsCtrl", function($scope, $rootScope) {	
-	$scope.posts = [
-	{
-		id: 1,
-		category: {
-			name: "Category Name"
-		},
-		creationDate: new Date(),
-		client: {
-			clientName: "Hanan"
-		},
-		comments: [{
-			client: {
-				clientName: "Hanan"
-			},
-			creationDate: new Date(),
-			content: "This is a long long content"
-		}],
-		title: "Post Title",
-		content: "This is a long long content"
-	}];
+app.controller("RecommendedPostsCtrl", function($scope, $rootScope, $window, httpFactory) {	
+	httpFactory.getRequest("/recomended_posts", function(data) {
+		$scope.posts = data.data;
+	});
+	// $scope.posts = [
+	// {
+	// 	id: 1,
+	// 	category: {
+	// 		name: "Category Name"
+	// 	},
+	// 	creationDate: new Date(),
+	// 	client: {
+	// 		clientName: "Hanan"
+	// 	},
+	// 	comments: [{
+	// 		client: {
+	// 			clientName: "Hanan"
+	// 		},
+	// 		creationDate: new Date(),
+	// 		content: "This is a long long content"
+	// 	}],
+	// 	title: "Post Title",
+	// 	content: "This is a long long content"
+	// }];
 });
